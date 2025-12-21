bcf9eabe89dbc07785c45a13e2304401cf116abc
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2025-12-21 10:22:08 +0100

    Navigation

---
+++ main.js
@@ -0,0 +1,39 @@
+const hero = document.querySelector(".hero");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  hero.style.setProperty("--parallax", `${scrollY * 0.4}px`);
+});
+
+
+// отслеживание скролла
+
+ const nav = document.querySelector("nav");
+
+ window.addEventListener("scroll", () => {
+   if (window.scrollY > 50) {
+     nav.classList.add("scrolled");
+   } else {
+     nav.classList.remove("scrolled");
+   }
+ });
+
+
+
+//  Скрывать nav при прокрутке вниз
+
+
+//    let lastScroll = 0;
+//    const nav = document.querySelector("nav");
+
+//    window.addEventListener("scroll", () => {
+//      const currentScroll = window.scrollY;
+
+//      if (currentScroll > lastScroll && currentScroll > 100) {
+//        nav.style.transform = "translate(-50%, 120%)"; // уезжает вниз
+//      } else {
+//        nav.style.transform = "translate(-50%, 0)";
+//      }
+
+//      lastScroll = currentScroll;
+//    });
