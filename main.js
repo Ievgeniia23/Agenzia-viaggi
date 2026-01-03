233903b69fb3646e2b6f4fbcd208a6615d84d5d7
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2026-01-03 07:32:54 +0100

    Correction sidebar and nav

--- main.js
+++ main.js
@@ -1,66 +1,54 @@
-const hero = document.querySelector(".hero");
+
+const nav = document.querySelector("nav");
+// const sidebar = document.querySelector(".sidebar");
+// const hero = document.querySelector(".hero");
+
 
 window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  hero.style.setProperty("--parallax", `${scrollY * 0.4}px`);
+  
+
+ 
+  
+if (window.scrollY > 50) {
+  nav.classList.add("is-fixed", "scrolled");
+  
+} else {
+  nav.classList.remove("is-fixed", "scrolled");
+  
+}
+    
+
+  // Sidebar
+  // if (sidebar && hero) {
+  //   const heroHeight = hero.offsetHeight;
+  //   if (scrollY > heroHeight - 100) {
+  //     sidebar.classList.add("is-sticky");
+  //   } else {
+  //     sidebar.classList.remove("is-sticky");
+  //   }
+  // }
 });
 
 
-// ---------------- NAV SCROLL -----------------
-
- const nav = document.querySelector("nav");
-
- window.addEventListener("scroll", () => {
-   if (window.scrollY > 50) {
-     nav.classList.add("scrolled");
-   } else {
-     nav.classList.remove("scrolled");
-   }
- });
-
-
-
-// -------------tour-carousel--------------------
-
+// Загрузка туров и инициализация слайдера
 fetch("./favourite-tours.json")
   .then((response) => response.json())
   .then((tours) => {
     const carousel = document.querySelector(".tour-carousel");
-
-    tours.forEach((tour) => {
-      carousel.innerHTML += `
-        <div class="tour-card">
-        <img src="${tour.img}" alt="${tour.title}" />
-          <h3>${tour.title}</h3>
-          <p>${tour.description}</p>
-        </div>
-      `;
-    });
-
-    initSlider();
+    if (carousel) {
+      tours.forEach((tour) => {
+        carousel.innerHTML += `
+          <div class="tour-card">
+            <img src="${tour.img}" alt="${tour.title}" />
+            <h3>${tour.title}</h3>
+            <p>${tour.description}</p>
+          </div>
+        `;
+      });
+      initSlider();
+    }
   })
   .catch((err) => console.error("Errore di caricamento:", err));
-
-
-//  $(document).ready(function () {
-//    $(".tour-carousel").slick({
-//      slidesToShow: 3,
-//      slidesToScroll: 1,
-//      dots: true,
-//      arrows: true,
-//      infinite: true,
-//      autoplay: true,
-//      autoplaySpeed: 3000,
-//      responsive: [
-//        {
-//          breakpoint: 768,
-//          settings: {
-//            slidesToShow: 1,
-//          },
-//        },
-//      ],
-//    });
-//  });
 
 function initSlider() {
   $(".tour-carousel").slick({
